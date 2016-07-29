a9be4bd1f97859030f64f810d2325481fd1a2fbe
AzureIoTClient <AzureIoTClient@localhost>
2016-07-29 16:01:07 -0700

    1.0.10

--- xlogging.c
+++ xlogging.c
@@ -4,7 +4,49 @@
 #include "azure_c_shared_utility/xlogging.h"
 #include "azure_c_shared_utility/consolelogger.h"
 
-static LOGGER_LOG global_log_function = consolelogger_log;
+#ifndef NO_LOGGING
+
+
+#ifdef WINCE
+#include <stdarg.h>
+#if defined _MSC_VER
+#define FUNC_NAME __FUNCDNAME__
+#else
+#define FUNC_NAME __func__
+#endif
+
+void consolelogger_log(LOG_CATEGORY log_category, unsigned int options, const char* format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	time_t t = time(NULL);
+
+	switch (log_category)
+	{
+	case LOG_INFO:
+		(void)printf("Info: ");
+		break;
+	case LOG_ERROR:
+		(void)printf("Error: Time:%.24s File:%s Func:%s Line:%d ", ctime(&t), __FILE__, FUNC_NAME, __LINE__);
+		break;
+	default:
+		break;
+	}
+
+	(void)vprintf(format, args);
+	va_end(args);
+
+	(void)log_category;
+	if (options & LOG_LINE)
+	{
+		(void)printf("\r\n");
+	}
+}
+#endif
+
+LOGGER_LOG global_log_function = consolelogger_log;
+
 
 void xlogging_set_log_function(LOGGER_LOG log_function)
 {
@@ -15,3 +57,5 @@ LOGGER_LOG xlogging_get_log_function(void)
 {
     return global_log_function;
 }
+
+#endif
